033eeea7180783ebe763859120dbd17e3ae22a39
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-08-01 21:33:39 +0100

    Fixed missing brackets (as they were commented out), handler function signature

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/fsouza/go-dockerclient"
+	"github.com/go-chi/chi"
 )
 
 type postStruct struct {
@@ -18,30 +20,34 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Route("/{userID}", func(r chi.Router) {
-	// gets data for {userID}'s instance
-	//r.Get("/", getData)
-	// creates an instance for {userID}
-	r.Post("/create", createContainer)
-	// updates {userID}'s instance
-	//r.Post("/update", updateContainer)
-	// updates the settings for {userID}'s instance. Settings sent in body
-	//.Put("/settings", updateSettings)
-	//})
+		// gets data for {userID}'s instance
+		//r.Get("/", getData)
+		// creates an instance for {userID}
+		r.Post("/create", createContainer)
+		// updates {userID}'s instance
+		//r.Post("/update", updateContainer)
+		// updates the settings for {userID}'s instance. Settings sent in body
+		//.Put("/settings", updateSettings)
+		//})
+	})
 }
 
 func getData() {}
 
-func createContainer(data postStruct) {
-	var userID = data.userID
+func createContainer(w http.ResponseWriter, r *http.Request) {
+	userID := "123"
 	endpoint := "unix:///var/run/docker.sock"
+
 	client, err := docker.NewClient(endpoint)
 	if err != nil {
 		panic(err)
 	}
+
 	containers, err := client.ListContainers(docker.ListContainersOptions{All: true})
 	if err != nil {
 		panic(err)
 	}
+
 	if containers != nil {
 		for i := 0; i < len(containers); i++ {
 			if containers[i].Names[0] == "/"+userID {
@@ -49,10 +55,12 @@ func createContainer(data postStruct) {
 			}
 		}
 	}
+
 	err = os.Mkdir("./users/"+userID, 0777)
 	if err != nil {
 		panic(err)
 	}
+
 	_, err = client.CreateContainer(docker.CreateContainerOptions{
 		Name: userID,
 		Config: &docker.Config{
@@ -66,7 +74,3 @@ func createContainer(data postStruct) {
 		panic(err)
 	}
 }
-
-func updateContainer() {}
-
-func updateSettings() {}
